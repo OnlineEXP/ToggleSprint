4963a610b67cf8344458b044e73ff4b17cc10efa
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2024-06-27 22:42:38 -0700

    Add files via upload

---
+++ ToggleSprint/index.js
@@ -0,0 +1,13 @@
+let on = true
+let f = new KeyBind(mc.field_71474_y.field_74351_w)
+let s = new KeyBind(mc.field_71474_y.field_151444_V)
+
+f.registerKeyDown(() => {
+    if (on) s.setState(true)
+});
+
+register("command", () => {
+    on = !on
+    if (on) ChatLib.chat("&aEnabled ToggleSprint")
+    else ChatLib.chat("&cDisabled ToggleSprint")
+}).setName("togglesprint").setAliases("ts")
